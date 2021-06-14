02d9c3c82d0196e7a4e6a2643905b0c39d8b54b8
Greg <gregori_monteiro@estudante.sc.senai.br>
2021-06-14 12:21:33 -0300

    PROJETO SA

---
+++ src/main/java/br/com/sistema/controller/SobreController.java
@@ -0,0 +1,15 @@
+package br.com.sistema.controller;
+
+//Seu principal objetivo é direcionar o fluxo da aplicação mapeando e direcionado as ações recebida (request)
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SobreController {
+
+    @GetMapping("/projeto/sobre")
+    public String sobre(){
+        System.out.println("Ok");
+        return "projeto/sobre";
+    }
+}
